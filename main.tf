257ca0189e8394c0424aab0e577b773770dffeaa
Dave Arnold <dave@roknsound.com>
2024-08-23 14:40:23 -0700

    Refactor main.tf to handle null values for source and destination usernames in image copying

--- main.tf
+++ main.tf
@@ -63,9 +63,9 @@ locals {
 
 
 resource "aws_ecr_repository" "image_repos" {
-  for_each = toset([for image in local.images : image.name])
-  name     = "${local.repo_parent_name}/${each.key}"
-
+  for_each             = toset([for image in local.images : image.name])
+  name                 = "${local.repo_parent_name}/${each.key}"
+  force_destroy        = true
   image_tag_mutability = var.image_tag_mutability
   image_scanning_configuration {
     scan_on_push = var.image_scan_on_push
